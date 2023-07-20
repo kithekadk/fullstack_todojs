c64a7d74a41c74bdae5b1308ec141c967bd90709
unknown <dankinyi99@gmail.com>
2023-07-20 14:01:31 +0300

    added configuration to use localhost

--- todo_backendjs/config/config.js
+++ todo_backendjs/config/config.js
@@ -6,7 +6,8 @@ const sqlConfig = {
     user: process.env.DB_USER,
     password: process.env.DB_PWD,
     database: process.env.DB_NAME,
-    server: 'tododb',
+    // server: 'tododb',
+    server: 'localhost',
     pool:{
         max: 10,
         min: 0,
--- todo_backendjs/controller/authController.js
+++ todo_backendjs/controller/authController.js
@@ -2,6 +2,7 @@ const {v4} = require ('uuid');
 const { sqlConfig } = require('../config/config');
 const mssql = require('mssql')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const registerUser = async(req, res)=>{
     try{
@@ -31,13 +32,31 @@ const loginUser = async(req, res)=>{
         const pool = await mssql.connect(sqlConfig);
 
         const user = (await pool.request().input('email', mssql.VarChar, email)
-        .execute('fetchUsers')).recordset
+        .execute('fetchUsers')).recordset[0]
 
-        // const decryptedPWD = bcrypt.compare()
+        hashedPwd = user.password
 
-        return res.status(200).json({message: user})
+        if(user){
+
+            const comparePwd = await bcrypt.compare(password, hashedPwd)
+
+        if(comparePwd){
+            
+            const {password, ...payload}= user
+            const token = jwt.sign(payload, process.env.SECRET, {expiresIn: '3600s'})
+            return res.status(200).json({
+                message: 'Logged in !!', token
+            })
+        }else{
+            return res.status(400).json({
+                message: 'Invalid login credentials'
+            })
+        }
+        }else{
+            return res.status(200).json({message: 'User not found'})
+        }
     } catch (error) {
-        
+        return res.status(400).json({error: error.message})
     }
 }
 
